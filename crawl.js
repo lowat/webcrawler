07d789031bc7ad53c9de868987f3fd1b3cb6a254
Logan Watkins <lw2014wa@gmail.com>
2024-04-01 14:22:07 -0500

    Update main.js to take cli args

--- crawl.js
+++ crawl.js
@@ -1,3 +1,27 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const aElements = dom.window.document.querySelectorAll('a')
+  for (const aElement of aElements){
+    if (aElement.href.slice(0,1) === '/'){
+      try {
+        urls.push(new URL(aElement.href, baseURL).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    }
+  }
+  return urls
+}
+
 function normalizeURL(url){
   const urlObj = new URL(url)
   let fullPath = `${urlObj.host}${urlObj.pathname}`
@@ -8,5 +32,6 @@ function normalizeURL(url){
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
